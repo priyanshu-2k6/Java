02c06b0e9c1bff3972600a86b5eb2fb3e1cc1d0e
Priyanshu Parate <folkjunglegaming@gmail.com>
2026-01-16 22:54:19 +0530

    Add program to find greatest of three numbers
    Introduced GreaterThanTwo.java, which reads three integers from user input and prints the greatest among them. Includes compiled class file.

--- prog-2/2nd/Quad.java
+++ prog-2/2nd/Quad.java
@@ -1,24 +1,23 @@
 import java.util.*;
 
-public class Quad{
-    public static void main(String[] args){
+public class Quad {
+    public static void main(String[] args) {
         Scanner s1 = new Scanner(System.in);
         System.out.println("This is the program to find the roots of the given quadratic equation ax^2 + bx + c = 0");
         System.out.println("Enter the values of a, b & c:");
         int a = s1.nextInt();
         int b = s1.nextInt();
         int c = s1.nextInt();
-        float d = (b*b) - (4*a*c);
-        if(d > 0){
-            double r1 = (-b + Math.sqrt(d)) / (2*a);
-            double r2 = (-b - Math.sqrt(d)) / (2*a);
+        float d = (b * b) - (4 * a * c);
+        if (d > 0) {
+            double r1 = (-b + Math.sqrt(d)) / (2 * a);
+            double r2 = (-b - Math.sqrt(d)) / (2 * a);
             String r1f = String.format("%.4f", r1);
             String r2f = String.format("%.4f", r2);
             System.out.println("The roots of the equation:- " + a + "x^2 + " + b + "x + " + c + " = 0 are: ");
             System.out.println("Root 1: " + r1f);
             System.out.println("Root 2: " + r2f);
-        }
-        else{
+        } else {
             System.out.println("The roots are imaginary.");
         }
         s1.close();
---
+++ prog-4/GreaterThanTwo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class GreaterThanTwo {
+
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+
+        System.out.println("Enter the 3 values below:");
+
+        int a = s1.nextInt();
+        int b = s1.nextInt();
+        int c = s1.nextInt();
+
+        int res = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+
+        System.out.println("The greates number among the three is: " + res);
+    }
+
+}
