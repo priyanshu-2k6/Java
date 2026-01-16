c60a704929c5ca1f3445cd9c7e608fac5f1a4ab2
Priyanshu Parate <folkjunglegaming@gmail.com>
2026-01-16 23:13:31 +0530

    Add Marks program to determine pass or fail
    Introduced Marks.java, a simple program that takes user input for marks and prints whether the user has passed or failed based on a threshold of 40.

---
+++ prog-5/Marks.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Marks {
+
+    public static void main(String[] args) {
+
+        Scanner s1 = new Scanner(System.in);
+
+        System.out.print("Enter your marks: ");
+        int a = s1.nextInt();
+
+        String res = a >= 40 ? "Passed" : "Failed";
+
+        System.out.println("The result is: " + res);
+        
+
+    }
+
+}
