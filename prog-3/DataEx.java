ab48cc6be3ec4e8ce07887c768d77581560d3f83
Priyanshu Parate <folkjunglegaming@gmail.com>
2026-01-16 22:33:45 +0530

    Add DataEx example demonstrating Java data types
    Introduces DataEx.java, a sample program that declares variables of various Java primitive data types and prints their values to the console. Useful for illustrating basic data type usage in Java.

---
+++ prog-3/DataEx.java
@@ -0,0 +1,25 @@
+public class DataEx {
+    public static void main(String[] args) {
+
+        byte day = 12;
+        short counts = 4556;
+        int income = 100000;
+        long population = 7800000000L;
+        float area = 14.45f;
+        double pi = 3.141592653589;
+        char letter = 'U';
+        char unicode = '\u0075';
+        boolean yes = false;
+
+        System.out.println("Byte value: " + day);
+        System.out.println("Short value: " + counts);
+        System.out.println("Int value: " + income);
+        System.out.println("Long value: " + population);
+        System.out.println("Float value: " + area);
+        System.out.println("Double value: " + pi);
+        System.out.println("Char value: " + letter);
+        System.out.println("Unicode Char value: " + unicode);
+        System.out.println("Boolean value: " + yes);
+
+    }
+}
