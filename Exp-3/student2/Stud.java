910b67b4b7617b5b06b23cb76c307d1e4a2593cc
Priyanshu Parate <folkjunglegaming@gmail.com>
2026-02-12 18:59:59 +0530

    Add Stud program with student class and search
    Introduce student and Stud.java: defines a student class with regNumber, Name, city, and methods set_profile, view_profile, and a static search by name. Stud.main creates an array of 3 student objects, reads profiles from Scanner, displays them, and performs a name search. Compiled .class files for student and Stud were also added.

---
+++ Exp-3/student2/Stud.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class student {
+  String regNumber;
+  String Name;
+  String city;
+
+  public void set_profile(Scanner input) {
+    System.out.print("Enter your regNumber: ");
+    this.regNumber = input.next();
+    System.out.print("Enter your Name: ");
+    this.Name = input.next();
+    System.out.print("Enter your City: ");
+    this.city = input.next();
+  }
+
+  public void view_profile() {
+    System.out.println(this.Name+" " + this.regNumber+" " + this.city);
+  }
+
+  public static void search(student t[], String inputName) {
+    boolean found = false;
+    for (int i = 0; i < t.length; i++) {
+      if (t[i].Name.equals(inputName)) {
+        System.out.println(t[i].regNumber +" "+ t[i].city);
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      System.out.println("No such student in the array !");
+    }
+  }
+}
+
+public class Stud {
+  public static void main(String args[]) {
+
+    Scanner input = new Scanner(System.in);
+
+    // declaring an array of objects for a class
+    student[] java = new student[3];
+
+    // allocation of memory for student objects
+    for (int i = 0; i < java.length; i++) {
+      java[i] = new student();
+    }
+
+    // calling the methods for each object
+    for (int i = 0; i < java.length; i++) {
+      java[i].set_profile(input);
+      java[i].view_profile();
+    }
+
+    // calling the search method
+    System.out.print("Enter the name to search: ");
+    String searchName = input.next();
+    student.search(java, searchName);
+
+    input.close();
+  }
+}
